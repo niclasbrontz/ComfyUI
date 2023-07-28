6b45242887cddaa7e1a88d3ce7aed78b1b7227e9
niclas <niclas.brontz@gmail.com>
2023-07-28 15:44:19 +0200

    Coninue working

--- flake.nix
+++ flake.nix
@@ -6,13 +6,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-#    comfyUi = {
-#      url = "github:comfyanonymous/ComfyUI";
-#      flake = false;
-#    };
+    comfyUi = {
+      url = "github:comfyanonymous/ComfyUI";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, comfyUi }:
     let
       system = "x86_64-linux";
 
@@ -45,6 +45,46 @@
       ];
 
     in {
+      packages."${system}".default = pkgs.stdenv.mkDerivation  {
+        name = "ComfyUI";
+
+        buildInputs = with pkgs; [
+          (python311.withPackages python-packages)
+        ];
+        
+#        unpackPhase = "cp ${./extra_model_paths.yaml} ${comfyUi}/extra_model_paths.yaml";
+        unpackPhase = ":";
+
+        installPhase = ''
+          # An ugly hack to make the patchshebang hook run for the python stuff
+          install -m755 -D ${./startComfyUI.py} $out/bin/startComfyUI.py
+          echo "
+          import sys
+
+          sys.path.insert(1, '${comfyUi}')
+
+          with open(\"${comfyUi}/main.py\") as f:
+            exec(f.read())" >> $out/bin/startComfyUI.py
+
+          # We have a few environment variables that needs to be specified
+          echo "
+          export PIP_PREFIX=$(pwd)/_build/pip_packages
+          export PYTHONPATH="$PIP_PREFIX/bin:$PATH"
+          unset SOURCE_DATE_EPOCH
+          $out/bin/startComfyUI.py " > $out/bin/startComfyUI.sh
+
+          chmod 755 $out/bin/startComfyUI.sh
+
+        '';
+
+        postPatch = "cp ${./extra_model_paths.yaml} ${comfyUi}/extra_model_paths.yaml";
+      };
+
+      apps."${system}".default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/startComfyUI.sh";
+      };
+
       devShells."${system}".default = pkgs.mkShell{
         name = "ComfyUI development";
 
@@ -56,6 +96,8 @@
 
         shellHook = ''
           echo "Staring $name"
+
+          echo "${comfyUi}"
 
           # Make the python virtual environment work
           export PIP_PREFIX=$(pwd)/_build/pip_packages
